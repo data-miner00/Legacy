17bb486d4d6a8814a30ef9f9debe657d8dfb801b
data-miner00 <data-miner00@users.noreply.github.com>
2024-10-26 18:22:57 +0800

    test: Add test project

---
+++ Legacy.Integrations.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Legacy.Integrations.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Legacy.Integrations.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3f8858f9-e60a-47ec-84f4-fc89fe2d7504")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Legacy.Integrations.UnitTests/UserRepositoryTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Legacy.Integrations.UnitTests
+{
+    using System;
+    using System.Threading.Tasks;
+    using Legacy.Core;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UserRepositoryTests
+    {
+        private readonly IUserRepository userRepository = new UserRepository();
+
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            var user = await this.userRepository.GetByIdAsync(1);
+
+            Assert.AreEqual("people@email.com", user.Email);
+            Assert.AreEqual("Jake", user.Name);
+        }
+    }
+}
